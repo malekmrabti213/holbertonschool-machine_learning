caf248f5f14b46bed580b6481a9b150fd95a1850
malekmrabti213 <malek6mrabti@gmail.com>
2023-03-17 11:08:59 +0100

    across2

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -7,8 +7,7 @@ def add_matrices2D(mat1, mat2):
             return None
         else:                  
             sumadd=[]
-            for j in range(len(mat1[i])):
-                
+            for j in range(len(mat1[i])):   
                 sumadd.append(mat1[i][j]+mat2[i][j])
             add.append(sumadd) 
     return add   
