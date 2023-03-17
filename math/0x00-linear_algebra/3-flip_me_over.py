ba01513b5c7a80357735b3a68f6fb2005d2af9ef
malekmrabti213 <malek6mrabti@gmail.com>
2023-03-17 11:03:26 +0100

    Flip2

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 def matrix_transpose(matrix):
-    
+
     matrix_transpose = []
     for index, row in enumerate(matrix):
-        if index is 0:
+        if index == 0:
             for i in row:
                 matrix_transpose.append([])
         for idx, i in enumerate(row):
