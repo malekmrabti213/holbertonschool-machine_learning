98fd6c313c6909e84719608510ff2bdfbccb954a
malekmrabti213 <malek6mrabti@gmail.com>
2023-05-01 22:25:41 +0100

    opt1

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def normalization_constants(X):
+    """creates normalization constants"""
+    m = np.mean(X, axis=0)
+    s = np.std(X, axis=0)
+    return m, s
