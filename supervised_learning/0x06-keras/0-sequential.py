44e77670ec6637aa007dbba9df123326066d5dce
malekmrabti213 <malek6mrabti@gmail.com>
2023-05-10 20:16:42 +0100

    is

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
 """
-Defines a function that builds a neural network
-using Keras library
+Task 0. Sequential
 """
-
-
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    Builds a neural network using Keras library
+    Function to build a neural network with the Keras library
 
-    parameters:
-        nx [int]: number of input features to the network
-        layers [list]:
-            contains the number of nodes in each layer of the network
-        activations [list]:
-            contains the activation functions used for each layer
-        lambtha [float]:
-            the L2 regularization parameter
-        keep_prob [float]:
-            the probability that a node will be kept for dropout
+        nx: number of input features to the network
+        layers: list containing the number of nodes in each layer of the
+        network
+        activations: list containing the activation functions used for each
+        layer of the network
+        lambtha: L2 regularization parameter
+        keep_prob: probability that a node will be kept for dropout
 
-    returns:
-        the keras model
+    Returns: the keras model
     """
-    sequential = []
-    shape = (nx,)
-    reg_l2 = K.regularizers.l2(lambtha)
+
+    # Create the model structure:
+    model = K.Sequential()
+
+    # Set the deep learning regularization technique:
+    regularizer = K.regularizers.L2(lambtha)
+
+    # Define the model architecture contemplating all layers, nodes and
+    # activation functions:
     for i in range(len(layers)):
         if i is 0:
-            sequential.append(K.layers.Dense(layers[i],
-                                             activation=activations[i],
-                                             kernel_regularizer=reg_l2,
-                                             input_shape=shape))
+            # First layer: receive nx input features:
+            model.add(K.layers.Dense(layers[i],
+                                     input_shape=(nx,),
+                                     activation=activations[i],
+                                     kernel_regularizer=regularizer))
         else:
-            sequential.append(K.layers.Dropout(1 - keep_prob))
-            sequential.append(K.layers.Dense(layers[i],
-                                             activation=activations[i],
-                                             kernel_regularizer=reg_l2))
-    model = K.Sequential(sequential)
+            # Add all other hidden layers with Dropout and L2 techniques:
+            model.add(K.layers.Dropout(1 - keep_prob))
+            model.add(K.layers.Dense(layers[i],
+                                     activation=activations[i],
+                                     kernel_regularizer=regularizer))
+
     return model
+
