0566c6bbe79c2e5e2e2830eba0f04fb0742af879
malekmrabti213 <malek6mrabti@gmail.com>
2023-05-22 14:12:20 +0100

    lenet_layers

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
-import tensorflow.keras as K
+import tensorflow as tf
+
+from tensorflow import keras as K
 
 def lenet5(X):
-    model = K.Sequential()
-    model.add(K.layers.Conv2D(6, 5, activation='relu', kernel_initializer='he_normal', padding='same', input_shape=X.shape[1:]))
-    model.add(K.layers.MaxPooling2D())
-    model.add(K.layers.Conv2D(16, 5, activation='relu', kernel_initializer='he_normal'))
-    model.add(K.layers.MaxPooling2D())
-    model.add(K.layers.Flatten())
-    model.add(K.layers.Dense(120, activation='relu', kernel_initializer='he_normal'))
-    model.add(K.layers.Dense(84, activation='relu', kernel_initializer='he_normal'))
-    model.add(K.layers.Dense(10, activation='softmax', kernel_initializer='he_normal'))
-
+    A1 = K.layers.Conv2D(6, 5, activation='relu', kernel_initializer='he_normal', padding='same')(X)
+    A2 = K.layers.MaxPooling2D()(A1)
+    A3 = K.layers.Conv2D(16, 5, activation='relu', kernel_initializer='he_normal')(A2)
+    A4 = K.layers.MaxPooling2D()(A3)
+    A5 = K.layers.Flatten()(A4)
+    A6 = K.layers.Dense(120, activation='relu', kernel_initializer='he_normal')(A5)
+    A7 = K.layers.Dense(84, activation='relu', kernel_initializer='he_normal')(A6)
+    Y = K.layers.Dense(10, activation='softmax', kernel_initializer='he_normal')(A7)
+    model = K.Model(inputs=X, outputs=Y)
     model.compile(optimizer=K.optimizers.Adam(),
                   loss='categorical_crossentropy', metrics=['accuracy'])
-    
     return model
