6eec29624684d3ddb951d1969d3f553778b3b1a6
malekmrabti213 <malek6mrabti@gmail.com>
2023-05-26 20:43:39 +0100

    newtask6

--- supervised_learning/0x08-deep_cnns/6-transition_layer.py
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-
 import tensorflow.keras as K
-transition_layer = __import__('6-transition_layer').transition_layer
 
-if __name__ == '__main__':
-    X = K.Input(shape=(56, 56, 256))
-    Y, nb_filters = transition_layer(X, 256, 0.5)
-    model = K.models.Model(inputs=X, outputs=Y)
-    model.summary()
-    print(nb_filters)
+def transition_layer(X, nb_filters, compression):
+
+    nb_filters = int(nb_filters * compression)
+
+    # 1x1 conv
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.Conv2D(nb_filters, (1, 1), padding='same', kernel_initializer=K.initializers.he_normal())(X)
+
+    # average pool
+    X = K.layers.AveragePooling2D((2, 2), strides=(2, 2), padding='same')(X)
+
+    return X, nb_filters
