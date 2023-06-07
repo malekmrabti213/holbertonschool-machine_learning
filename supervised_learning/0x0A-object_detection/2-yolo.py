03447c01920a535a8f2a731886dbde870f53e4ac
malekmrabti213 <malek6mrabti@gmail.com>
2023-06-07 23:57:25 +0100

    test11

--- supervised_learning/0x0A-object_detection/2-yolo.py
+++ supervised_learning/0x0A-object_detection/2-yolo.py
@@ -125,29 +125,15 @@ class Yolo:
             for each box in filtered_boxes, respectively
         """
         # Flatten the boxes, box_confidences, and box_class_probs lists
-        boxes = [box.reshape(-1, 4) for box in boxes]
-        box_confidences = [
-            box_confidence.reshape(-1, 1) for box_confidence in box_confidences
-            ]
-        box_class_probs = [
-            box_class_prob.reshape(
-                -1,
-                len(self.class_names)) for box_class_prob in box_class_probs
-            ]
+        boxes = np.concatenate([box.reshape(-1, 4) for box in boxes])
+        box_confidences = np.concatenate([box_confidence.reshape(-1, 1) for box_confidence in box_confidences])
+        box_class_probs = np.concatenate([box_class_prob.reshape(-1, len(self.class_names)) for box_class_prob in box_class_probs])
 
-        # Concatenate the flattened arrays
-        boxes = np.concatenate(boxes)
-        box_confidences = np.concatenate(box_confidences)
-        box_class_probs = np.concatenate(box_class_probs)
-
-        # Find the box scores by multiplying box_confidences with
-        # box_class_probs
-   
+        # Find the box scores by multiplying box_confidences with box_class_probs
         box_scores = box_confidences * box_class_probs
 
         # Filter boxes based on box_scores and class threshold
-        box_mask = box_scores >= self.class_t
-        box_mask = np.where(box_mask, axis=-1)
+        box_mask = np.any(box_scores >= self.class_t, axis=-1)
         filtered_boxes = boxes[box_mask]
         box_classes = np.argmax(box_scores, axis=-1)[box_mask]
         box_scores = np.max(box_scores, axis=-1)[box_mask]
