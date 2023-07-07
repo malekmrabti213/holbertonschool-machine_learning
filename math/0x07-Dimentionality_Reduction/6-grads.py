27077ebdfed5f10d98b7fc01f3606f2f9c3f4db2
malekmrabti213 <malek6mrabti@gmail.com>
2023-07-07 18:57:28 +0100

    grad fix

--- math/0x07-Dimentionality_Reduction/6-grads.py
+++ math/0x07-Dimentionality_Reduction/6-grads.py
@@ -4,29 +4,15 @@ import numpy as np
 Q_affinities = __import__('5-Q_affinities').Q_affinities
 
 
-# def grads(Y, P):
-#     """calculates the gradients of Y"""
-#     n, ndims = Y.shape
-#     dY = np.zeros((n, ndims))
-#     Q, num = Q_affinities(Y)
-#     PQ = P - Q
-#     for i in range(n):
-#         part = np.expand_dims((PQ[:, i] * num[:, i]).T, axis=-1)
-#         dY[i, :] = np.sum(part * (Y[i, :] - Y), 0)
-#     return dY, Q
-def grads(Y, Pij):
-    """
-    Compute the grads
-    :param Y: Containing the low dimensional transformation of X
-    :param Pij: The Pij for all ij affinities
-    :return: The grads
-    """
+def grads(Y, P):
+    """calculates the gradients of Y"""
     n, ndims = Y.shape
+    dY = np.zeros((n, ndims))
+    Q, num = Q_affinities(Y)
+    PQ = P - Q
+    for i in range(n):
+        part = np.expand_dims((PQ[:, i] * num[:, i]).T, axis=-1)
+        dY[i, :] = np.sum(part * (Y[i, :] - Y), 0)
+    return dY, Q
 
-    Qij, numerator = Q_affinities(Y)
-    PQij = Pij - Qij
-
-    dY = np.dot(PQij * numerator.T, Y - np.tile(Y, (n, 1)))
-
-    return -dY, Qij
 
