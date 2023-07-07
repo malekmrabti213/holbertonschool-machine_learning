6166dae30bcf038c5d6a2edd0646871c0f057f49
malekmrabti213 <malek6mrabti@gmail.com>
2023-07-07 18:14:50 +0100

    update grad

--- math/0x07-Dimentionality_Reduction/6-grads.py
+++ math/0x07-Dimentionality_Reduction/6-grads.py
@@ -4,13 +4,26 @@ import numpy as np
 Q_affinities = __import__('5-Q_affinities').Q_affinities
 
 
-def grads(Y, P):
-    """calculates the gradients of Y"""
-    n, ndims = Y.shape
-    dY = np.zeros((n, ndims))
-    Q, num = Q_affinities(Y)
-    PQ = P - Q
-    for i in range(n):
-        part = np.expand_dims((PQ[:, i] * num[:, i]).T, axis=-1)
-        dY[i, :] = np.sum(part * (Y[i, :] - Y), 0)
-    return dY, Q
+# def grads(Y, P):
+#     """calculates the gradients of Y"""
+#     n, ndims = Y.shape
+#     dY = np.zeros((n, ndims))
+#     Q, num = Q_affinities(Y)
+#     PQ = P - Q
+#     for i in range(n):
+#         part = np.expand_dims((PQ[:, i] * num[:, i]).T, axis=-1)
+#         dY[i, :] = np.sum(part * (Y[i, :] - Y), 0)
+#     return dY, Q
+def grads(Y, Pij):
+    """
+    Compute the grads
+    :param Y: Containing the low dimensional transformation of X
+    :param Pij: The Pij for all ij affinities
+    :return: The grads
+    """
+    Qij, numerator = Q_affinities(Y)
+    PQij = Pij - Qij
+
+    dY = np.sum((PQij * numerator[:, :, np.newaxis]) * (Y[np.newaxis, :] - Y[:, np.newaxis]), axis=1)
+
+    return -dY, Qij
