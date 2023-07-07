4ab9223c4768b6133776bea340c46ced62cb26fd
malekmrabti213 <malek6mrabti@gmail.com>
2023-07-07 15:55:01 +0100

    m

--- math/0x07-Dimentionality_Reduction/8-tsne.py
+++ math/0x07-Dimentionality_Reduction/8-tsne.py
@@ -7,27 +7,44 @@ grads = __import__('6-grads').grads
 cost = __import__('7-cost').cost
 
 
-def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
-    """Calculates the t-SNE transformation"""
-    X = pca(X, idims).real
-    n = X.shape[0]
-    momentum = 0.5
-    final_momentum = 0.8
-    Y = np.random.randn(n, ndims)
-    iY = np.zeros((n, ndims))
 
-    P = P_affinities(X, 1e-5, perplexity)
-    P = 4 * P
-    for iter in range(iterations):
-        dY, Q = grads(Y, P)
-        if iter == 20:
-            momentum = final_momentum
-        iY = momentum * iY - lr * dY
-        Y = Y + iY
-        Y = Y - np.mean(Y, 0)
-        if (iter + 1) % 100 == 0:
-            C = cost(P, Q)
-            print("Cost at iteration {}: {}".format(iter + 1, C))
-        if iter + 1 == 100:
-            P = P / 4
-    return Y
+def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
+    """
+    Compute the t-sne dimension reduction
+    :param X: Containing the dataset to be transformed by t-SNE
+    :param ndims: New dimensional representation of X
+    :param idims: Intermediate dimensional representation of X after PCA
+    :param perplexity: Is the perplexity
+    :param iterations: Is the number of iterations
+    :param lr: Is the learning rate
+    :return: Y containing the optimized low dimensional transformation of X
+    """
+    momentum_coeff = 0.8
+    n, d = X.shape
+
+    pca_res = pca(X, idims)
+    P = P_affinities(pca_res, perplexity=perplexity)
+    P *= 4
+
+    Y = []
+    y = np.random.randn(n, ndims)
+    Y.append(y)
+    Y.append(y)
+
+    for i in range(iterations):
+        dY, Q = grads(Y[-1], P)
+        y = Y[-1] - lr * dY + momentum_coeff * (Y[-1] - Y[-2])
+        y = y - np.tile(np.mean(y, 0), (n, 1))
+        Y.append(y)
+
+        if (i + 1) % 100 == 0:
+            current_cost = cost(P, Q)
+            print("Cost at iteration {}: {}".format(i + 1, current_cost))
+
+        if i == 20:
+            momentum_coeff = 0.5
+
+        if (i + 1) == 100:
+            P /= 4
+
+    return Y[-1]
