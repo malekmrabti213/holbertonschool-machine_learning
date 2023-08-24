df68316aa5679be6f3949c4d79db322a84c54074
malekmrabti213 <malek6mrabti@gmail.com>
2023-08-24 11:36:39 +0100

    task 0

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -1,12 +1,3 @@
-# #!/usr/bin/env python3
-
-# import tensorflow as tf
-
-# def create_placeholders(nx, classes):
-#     """create placeholders for a neural network classifier"""
-#     x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
-#     y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
-#     return x, y
 #!/usr/bin/env python3
 """
 module containing function create_placeholders
