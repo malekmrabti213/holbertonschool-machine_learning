0e0967a7dbd451da3cd580b75c350c14daee9604
malekmrabti213 <malek6mrabti@gmail.com>
2023-08-24 11:35:11 +0100

    tensorflow task 0 mari

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -1,9 +1,28 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-import tensorflow as tf
+# import tensorflow as tf
+
+# def create_placeholders(nx, classes):
+#     """create placeholders for a neural network classifier"""
+#     x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
+#     y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+#     return x, y
+#!/usr/bin/env python3
+"""
+module containing function create_placeholders
+"""
+import tensorflow.compat.v1 as tf
+
 
 def create_placeholders(nx, classes):
-    """create placeholders for a neural network classifier"""
-    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
-    y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+    """
+    function that returns two placeholders, x and y, for the neural network
+    Args:
+        nx: the number of feature columns in our data
+        classes: the number of classes in our classifier
+    Return: two placeholders, x and y
+    """
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
+
     return x, y
