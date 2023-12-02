5013d35e77d9b590a72f40120460d00e28969f23
malekmrabti213 <malek6mrabti@gmail.com>
2023-12-02 01:44:22 +0100

    task 4

---
+++ supervised_learning/decision_tree/4-build_decision_tree.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self) :
+        if self.is_leaf:
+            return self.depth
+        else:
+            left_depth = self.left_child.max_depth_below()
+            right_depth = self.right_child.max_depth_below()
+            return max(left_depth, right_depth)
+        
+    def count_nodes_below(self, only_leaves=False):
+        count = 0
+        if not only_leaves:
+            count += 1
+        if self.left_child:
+            count += self.left_child.count_nodes_below(only_leaves)
+        if self.right_child:
+            count += self.right_child.count_nodes_below(only_leaves)
+        return count
+    
+    def left_child_add_prefix(self,text):
+        lines=text.split("\n")
+        new_text="    +--"+lines[0]+"\n"
+        for x in lines[1:] :
+            new_text+=("    |  "+x)+"\n"
+
+        return (new_text)
+
+    def right_child_add_prefix(self,text):
+        lines = text.split("\n")
+        new_text = "    +--" + lines[0] + "\n"
+        for x in lines[1:]:
+            new_text += ("       " + x) + "\n"
+
+        return (new_text)
+
+    def __str__(self):
+        if self.is_root :
+            a = self.left_child_add_prefix(f"{self.left_child}"[:-1])
+            b = self.right_child_add_prefix(f"{self.right_child}"[:-1])
+            return f"root [feature={self.feature}, threshold={self.threshold}]\n" \
+                   f"{a}{b}"
+
+
+        else:
+            a=self.left_child_add_prefix(f"{self.left_child}"[:-1])
+            b=self.right_child_add_prefix(f"{self.right_child}"[:-1])
+            return f"-> node [feature={self.feature}, threshold={self.threshold}]\n"\
+                   f"{a}{b}"
+    
+    def get_leaves_below(self) :
+        return self.left_child.get_leaves_below()+self.right_child.get_leaves_below()
+    
+    def update_bounds_below(self) :
+        if self.is_root : 
+            self.upper = { 0:np.inf }
+            self.lower = {0 : -1*np.inf }
+                        
+        for child in [self.left_child, self.right_child] :
+            child.upper = self.upper.copy()
+            child.lower = self.lower.copy()
+        if self.feature in self.left_child.lower.keys() :
+            self.left_child.lower[self.feature]=max(self.threshold,self.left_child.lower[self.feature])
+        else :
+            self.left_child.lower[self.feature]=self.threshold
+        if self.feature in self.right_child.upper.keys() :
+            self.right_child.upper[self.feature]=min(self.threshold,self.right_child.upper[self.feature])
+        else :
+            self.right_child.upper[self.feature]=self.threshold
+                
+        for child in [self.left_child, self.right_child] :
+            child.update_bounds_below()    
+
+
+class Leaf(Node):
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self) :
+        return self.depth
+    
+    def count_nodes_below(self, only_leaves=False) :
+        return 1
+    
+    def __str__(self):
+        return (f"-> leaf [value={self.value}] ")
+    
+    def get_leaves_below(self) :
+        return [self]
+    
+    def update_bounds_below(self) :
+        pass    
+    
+class Decision_Tree():
+    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self) :
+        return self.root.max_depth_below()
+    
+    def count_nodes(self, only_leaves=False) :
+        return self.root.count_nodes_below(only_leaves=only_leaves)
+    
+    def __str__(self):
+        return self.root.__str__()
+    
+    def get_leaves(self) :
+        return self.root.get_leaves_below()
+    
+    def update_bounds(self) :
+        self.root.update_bounds_below() 
