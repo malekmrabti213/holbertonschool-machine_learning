a53a9a83633ba779109a872b76cda1ec50696cc5
malekmrabti213 <malek6mrabti@gmail.com>
2024-02-29 15:23:54 +0100

    dt

--- supervised_learning/decision_tree/1-build_decision_tree.py
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class Node:
     def __init__(self, feature=None, threshold=None, left_child=None,
-                  right_child=None, is_root=False, depth=0):
+                 right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -50,7 +50,7 @@ class Leaf(Node):
 
 class Decision_Tree:
     def __init__(self, max_depth=10, min_pop=1, seed=0,
-                  split_criterion="random", root=None):
+                 split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -68,4 +68,3 @@ class Decision_Tree:
 
     def count_nodes(self, only_leaves=False):
         return self.root.count_nodes_below(only_leaves=only_leaves)
-    
