e21e21dcf61e8c8fa73c8ec4cfe8816ecdce7934
malekmrabti213 <malek6mrabti@gmail.com>
2024-02-29 16:12:54 +0100

    dt2

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """
 """
+
 import numpy as np
+
 
 class Node:
     """
     """
+
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
         """
@@ -19,7 +22,7 @@ class Node:
         self.sub_population = None
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         """
         """
         if self.is_leaf:
@@ -28,7 +31,7 @@ class Node:
             left_depth = self.left_child.max_depth_below()
             right_depth = self.right_child.max_depth_below()
             return max(left_depth, right_depth)
-        
+
     def count_nodes_below(self, only_leaves=False):
         """
         """
@@ -40,18 +43,18 @@ class Node:
         if self.right_child:
             count += self.right_child.count_nodes_below(only_leaves)
         return count
-    
-    def left_child_add_prefix(self,text):
+
+    def left_child_add_prefix(self, text):
         """
         """
-        lines=text.split("\n")
-        new_text="    +--"+lines[0]+"\n"
-        for x in lines[1:] :
-            new_text+=("    |  "+x)+"\n"
+        lines = text.split("\n")
+        new_text = "    +--" + lines[0] + "\n"
+        for x in lines[1:]:
+            new_text += ("    |  " + x) + "\n"
 
-        return (new_text)
+        return new_text
 
-    def right_child_add_prefix(self,text):
+    def right_child_add_prefix(self, text):
         """
         """
         lines = text.split("\n")
@@ -59,27 +62,27 @@ class Node:
         for x in lines[1:]:
             new_text += ("       " + x) + "\n"
 
-        return (new_text)
+        return new_text
 
     def __str__(self):
         """
         """
-        if self.is_root :
+        if self.is_root:
             a = self.left_child_add_prefix(f"{self.left_child}"[:-1])
             b = self.right_child_add_prefix(f"{self.right_child}"[:-1])
             return f"root [feature={self.feature}, threshold={self.threshold}]\n" \
                    f"{a}{b}"
-
-
         else:
-            a=self.left_child_add_prefix(f"{self.left_child}"[:-1])
-            b=self.right_child_add_prefix(f"{self.right_child}"[:-1])
+            a = self.left_child_add_prefix(f"{self.left_child}"[:-1])
+            b = self.right_child_add_prefix(f"{self.right_child}"[:-1])
             return f"-> node [feature={self.feature}, threshold={self.threshold}]\n"\
                    f"{a}{b}"
-    
+
+
 class Leaf(Node):
     """
     """
+
     def __init__(self, value, depth=None):
         """
         """
@@ -88,24 +91,26 @@ class Leaf(Node):
         self.is_leaf = True
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         """
         """
         return self.depth
-    
-    def count_nodes_below(self, only_leaves=False) :
+
+    def count_nodes_below(self, only_leaves=False):
         """
         """
         return 1
-    
+
     def __str__(self):
         """
         """
         return (f"-> leaf [value={self.value}] ")
 
-class Decision_Tree():
+
+class Decision_Tree:
     """
     """
+
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
@@ -122,18 +127,17 @@ class Decision_Tree():
         self.split_criterion = split_criterion
         self.predict = None
 
-    def depth(self) :
+    def depth(self):
         """
         """
         return self.root.max_depth_below()
-    
-    def count_nodes(self, only_leaves=False) :
+
+    def count_nodes(self, only_leaves=False):
         """
         """
         return self.root.count_nodes_below(only_leaves=only_leaves)
-    
+
     def __str__(self):
         """
         """
         return self.root.__str__()
-    
