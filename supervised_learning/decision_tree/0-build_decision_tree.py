07273770f637594d8ce0a4fdc87cc0c78a92ca7b
malekmrabti213 <malek6mrabti@gmail.com>
2024-02-29 15:51:58 +0100

    dt0

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -2,8 +2,14 @@
 
 import numpy as np
 
+
 class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    """
+    """
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        """
+        """
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -12,7 +18,10 @@ class Node:
         self.is_root = is_root
         self.sub_population = None
         self.depth = depth
-    def max_depth_below(self) :
+
+    def max_depth_below(self):
+        """
+        """
         if self.is_leaf:
             return self.depth
         else:
@@ -20,18 +29,29 @@ class Node:
             right_depth = self.right_child.max_depth_below()
             return max(left_depth, right_depth)
 
+
 class Leaf(Node):
+    """
+    """
     def __init__(self, value, depth=None):
         super().__init__()
+        """
+        """
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         return self.depth
 
-class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+
+class DecisionTree:
+    """
+    """
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        """
+        """
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -44,5 +64,8 @@ class Decision_Tree():
         self.split_criterion = split_criterion
         self.predict = None
 
-    def depth(self) :
+    def depth(self):
+        """
+        """
         return self.root.max_depth_below()
+
