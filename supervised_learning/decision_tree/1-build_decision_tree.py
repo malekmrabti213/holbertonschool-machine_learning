0530469ad9aabdf01eae6b4e46cb031333f87cca
malekmrabti213 <malek6mrabti@gmail.com>
2024-02-29 15:34:24 +0100

    dt 1

--- supervised_learning/decision_tree/1-build_decision_tree.py
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -6,12 +6,12 @@ import numpy as np
 
 
 class Node:
-    """
-    Class Node
-    """
+    """Class Node"""
 
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
+        """
+        """
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -22,6 +22,8 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
+        """
+        """
         if self.is_leaf:
             return self.depth
         else:
@@ -30,6 +32,8 @@ class Node:
             return max(left_depth, right_depth)
 
     def count_nodes_below(self, only_leaves=False):
+        """
+        """
         count = 0
         if not only_leaves:
             count += 1
@@ -41,30 +45,34 @@ class Node:
 
 
 class Leaf(Node):
-    """
-    class Leaf
-    """
+    """class Leaf"""
 
     def __init__(self, value, depth=None):
         super().__init__()
+        """
+        """
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
     def max_depth_below(self):
+        """
+        """
         return self.depth
 
     def count_nodes_below(self, only_leaves=False):
+        """
+        """
         return 1
 
 
 class Decision_Tree:
-    """
-    class decision tree
-    """
+    """class decision tree"""
 
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
+        """
+        """
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -78,7 +86,11 @@ class Decision_Tree:
         self.predict = None
 
     def depth(self):
+        """
+        """
         return self.root.max_depth_below()
 
     def count_nodes(self, only_leaves=False):
+        """
+        """
         return self.root.count_nodes_below(only_leaves=only_leaves)
