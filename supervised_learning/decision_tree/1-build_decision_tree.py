49abf5dac5ce2110aa108fe13b8d1f5645d4dcab
malekmrabti213 <malek6mrabti@gmail.com>
2024-02-29 15:17:07 +0100

    dt

--- supervised_learning/decision_tree/1-build_decision_tree.py
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 class Node:
     def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
         self.feature = feature
@@ -13,14 +14,14 @@ class Node:
         self.sub_population = None
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         if self.is_leaf:
             return self.depth
         else:
             left_depth = self.left_child.max_depth_below()
             right_depth = self.right_child.max_depth_below()
             return max(left_depth, right_depth)
-        
+
     def count_nodes_below(self, only_leaves=False):
         count = 0
         if not only_leaves:
@@ -30,7 +31,8 @@ class Node:
         if self.right_child:
             count += self.right_child.count_nodes_below(only_leaves)
         return count
-    
+
+
 class Leaf(Node):
     def __init__(self, value, depth=None):
         super().__init__()
@@ -38,13 +40,14 @@ class Leaf(Node):
         self.is_leaf = True
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         return self.depth
-    
-    def count_nodes_below(self, only_leaves=False) :
+
+    def count_nodes_below(self, only_leaves=False):
         return 1
 
-class Decision_Tree():
+
+class Decision_Tree:
     def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
@@ -58,8 +61,8 @@ class Decision_Tree():
         self.split_criterion = split_criterion
         self.predict = None
 
-    def depth(self) :
+    def depth(self):
         return self.root.max_depth_below()
-    
-    def count_nodes(self, only_leaves=False) :
+
+    def count_nodes(self, only_leaves=False):
         return self.root.count_nodes_below(only_leaves=only_leaves)
