58fbc34160de2896e4227cc9e980c3cfa0295b79
malekmrabti213 <malek6mrabti@gmail.com>
2024-03-01 16:44:53 +0100

    dt5

--- supervised_learning/decision_tree/5-build_decision_tree.py
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -112,25 +112,25 @@ class Node:
         for child in [self.left_child, self.right_child]:
             child.update_bounds_below()
 
-def update_indicator(self):
-    """
-    """
-    def is_large_enough(x):
-        return np.all(
-            np.array([np.greater(x[:, key], self.lower[key]) for key in self.lower]),
+    def update_indicator(self):
+        """
+        """
+        def is_large_enough(x):
+            return np.all(
+                np.array([np.greater(x[:, key], self.lower[key]) for key in self.lower]),
+                axis=0
+            )
+        
+        def is_small_enough(x):
+            return np.all(
+                np.array([np.less_equal(x[:, key], self.upper[key]) for key in self.upper]),
+                axis=0
+            )
+        
+        self.indicator = lambda x: np.all(
+            np.array([is_large_enough(x), is_small_enough(x)]),
             axis=0
         )
-    
-    def is_small_enough(x):
-        return np.all(
-            np.array([np.less_equal(x[:, key], self.upper[key]) for key in self.upper]),
-            axis=0
-        )
-    
-    self.indicator = lambda x: np.all(
-        np.array([is_large_enough(x), is_small_enough(x)]),
-        axis=0
-    )
 
 
 class Leaf(Node):
