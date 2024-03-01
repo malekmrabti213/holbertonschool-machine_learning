f2b0f9e8db513bf96e2d074772d80f069743d923
malekmrabti213 <malek6mrabti@gmail.com>
2024-03-01 13:39:15 +0100

    dt

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -80,7 +80,7 @@ class Node:
             return f"-> node [feature={feature}, threshold={threshold}]\n"\
                    f"{a}{b}"
 
-class Leaf(Node):
+class Leaf(Node): 
     """class Leaf"""
 
     def __init__(self, value, depth=None):
