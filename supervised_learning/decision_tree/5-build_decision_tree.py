04a9710027b37c52868f0fe1f81ba3b134a0d334
malekmrabti213 <malek6mrabti@gmail.com>
2024-03-01 15:57:11 +0100

    dt5

--- supervised_learning/decision_tree/5-build_decision_tree.py
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -116,12 +116,17 @@ class Node:
         """
         """
         def is_large_enough(x):
-            return np.all(np.array([np.greater(x[:, key], self.lower[key]) for key in list(self.lower.keys())]), axis=0)
-        
+            """
+            """
+            return np.all(np.array([np.greater(x[:, key], self.lower[key]) \
+                                    for key in list(self.lower.keys())]), axis=0)        
         def is_small_enough(x):
-            return np.all(np.array([np.less_equal(x[:, key], self.upper[key]) for key in list(self.upper.keys())]), axis=0)
-        
-        self.indicator = lambda x: np.all(np.array([is_large_enough(x), is_small_enough(x)]), axis=0)
+            """
+            """
+            return np.all(np.array([np.less_equal(x[:, key], self.upper[key]) \
+                                    for key in list(self.upper.keys())]), axis=0)        
+        self.indicator = lambda x: np.all(np.array([is_large_enough(x), \
+                                                    is_small_enough(x)]), axis=0)
 
 
 class Leaf(Node):
