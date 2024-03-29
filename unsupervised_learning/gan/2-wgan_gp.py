1258a93905a8b473710f7ebc7873271882f047ec
malekmrabti213 <malek6mrabti@gmail.com>
2024-03-29 17:48:20 +0100

    task2-gan

--- unsupervised_learning/gan/2-wgan_gp.py
+++ unsupervised_learning/gan/2-wgan_gp.py
@@ -2,19 +2,21 @@
 """
 Task 2
 """
+
 import tensorflow as tf
 from tensorflow import keras
 
 class WGAN_GP(keras.Model):
     """
     """
+
     def __init__(self, generator, discriminator,
                  latent_generator, real_examples,
                  batch_size=200, disc_iter=2,
                  learning_rate=0.005, lambda_gp=10):
         super().__init__()
         """
-        """                        
+        """
         self.latent_generator = latent_generator
         self.real_examples = real_examples
         self.generator = generator
@@ -23,18 +25,18 @@ class WGAN_GP(keras.Model):
         self.disc_iter = disc_iter
 
         self.learning_rate = learning_rate
-        self.beta_1 = 0.3                              
-        self.beta_2 = 0.9                              
+        self.beta_1 = 0.3
+        self.beta_2 = 0.9
 
-        self.lambda_gp = lambda_gp                                
-        self.dims = self.real_examples.shape                             
-        self.len_dims = tf.size(self.dims)                                 
-        self.axis = tf.range(1, self.len_dims, delta=1, dtype='int32')   
-        self.scal_shape = self.dims.as_list()                              
-        self.scal_shape[0] = self.batch_size                               
-        for i in range(1, self.len_dims):                                 
-            self.scal_shape[i] = 1                                         
-        self.scal_shape = tf.convert_to_tensor(self.scal_shape)   
+        self.lambda_gp = lambda_gp
+        self.dims = self.real_examples.shape
+        self.len_dims = tf.size(self.dims)
+        self.axis = tf.range(1, self.len_dims, delta=1, dtype='int32')
+        self.scal_shape = self.dims.as_list()
+        self.scal_shape[0] = self.batch_size
+        for i in range(1, self.len_dims):
+            self.scal_shape[i] = 1
+        self.scal_shape = tf.convert_to_tensor(self.scal_shape)
 
         # define the generator loss and optimizer:
         self.generator.loss = lambda x: -tf.reduce_mean(x)
@@ -72,7 +74,7 @@ class WGAN_GP(keras.Model):
         random_indices = tf.random.shuffle(sorted_indices)[:size]
         return tf.gather(self.real_examples, random_indices)
 
-    # generator of interpolating samples of size batch_size              
+    # generator of interpolating samples of size batch_size
     def get_interpolated_sample(self, real_sample, fake_sample):
         """
         """
@@ -80,7 +82,7 @@ class WGAN_GP(keras.Model):
         v = tf.ones(self.scal_shape) - u
         return u * real_sample + v * fake_sample
 
-    # computing the gradient penalty                                     
+    # computing the gradient penalty
     def gradient_penalty(self, interpolated_sample):
         """
         """
@@ -112,7 +114,7 @@ class WGAN_GP(keras.Model):
                 interpolated_sample = self.get_interpolated_sample(
                     real_sample, fake_sample)
 
-                # compute the loss of the discriminator on real and fake samples
+                # the loss of the discriminator on real & fake samples
                 discr_return_on_fake = self.discriminator(fake_sample,
                                                           training=True)
                 discr_return_on_real = self.discriminator(real_sample,
