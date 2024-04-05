b88e6be0a40d972a46458e522cb4a3f647275c8f
malekmrabti213 <malek6mrabti@gmail.com>
2024-04-06 00:09:57 +0100

    plot

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,14 +1,3 @@
-
-The linting feedback you received is about the spacing and trailing whitespace in your Python code. Here's how you can address it:
-
-Expected 2 blank lines: This means there should be two blank lines before the function definition. You can simply add an additional blank line before the def line(): line.
-
-Trailing whitespace: This means there are extra spaces at the end of some lines. Remove these extra spaces.
-
-After addressing these issues, your code should look something like this:
-
-python
-Copy code
 #!/usr/bin/env python3
 """
 Task 0
