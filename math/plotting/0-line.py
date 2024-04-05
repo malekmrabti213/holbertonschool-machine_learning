2ab9255ea0dc43b74df92a9c9b2113df4c5f9c43
malekmrabti213 <malek6mrabti@gmail.com>
2024-04-05 23:41:53 +0100

    plot

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+"""
+Task 0
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def line():
+    """
+    """   
     y = np.arange(0, 11) ** 3
     plt.plot(y, color='red')
     plt.xlim(0, 10) 
