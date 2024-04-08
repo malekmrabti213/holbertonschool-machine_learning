a6b2eb001c2edd6d8863e1185ecd2cfe22b428a7
malekmrabti213 <malek6mrabti@gmail.com>
2024-04-08 15:23:36 +0100

    task5

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -2,6 +2,7 @@
 """
 Task 5
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -32,50 +33,45 @@ def all_in_one():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
-    # your code here
     fig = plt.figure(figsize=(6, 6))
     fig.suptitle('All in One')
     spec = fig.add_gridspec(3, 2, wspace=0.4, hspace=0.5)
 
     ax10 = fig.add_subplot(spec[0, 0])
-    plt.xlim(0, 10)
-    plt.plot(y0, color='red')
-
+    ax10.plot(y0, color='red')
+    ax10.set_xlim(0, 10)
 
     ax11 = fig.add_subplot(spec[0, 1])
-    plt.xlabel('Height (in)', size='x-small')
-    plt.ylabel('Weight (lbs)', size='x-small')
-    plt.title("Men's Height vs Weight", size='x-small')
-    plt.scatter(x1, y1, c='magenta')
-
+    ax11.scatter(x1, y1, c='magenta')
+    ax11.set_xlabel('Height (in)', size='x-small')
+    ax11.set_ylabel('Weight (lbs)', size='x-small')
+    ax11.set_title("Men's Height vs Weight", size='x-small')
 
     ax20 = fig.add_subplot(spec[1, 0])
-    plt.xlabel('Time (years)',size='x-small')
-    plt.ylabel('Fraction Remaining',size='x-small')
-    plt.title("Exponential Decay of C-14",size='x-small')
-    plt.yscale("log")
-    plt.xlim(0, 28650)
-    plt.plot(x2, y2)
-
+    ax20.plot(x2, y2)
+    ax20.set_xlabel('Time (years)', size='x-small')
+    ax20.set_ylabel('Fraction Remaining', size='x-small')
+    ax20.set_title("Exponential Decay of C-14", size='x-small')
+    ax20.set_xlim(0, 28650)
+    ax20.set_yscale("log")
 
     ax21 = fig.add_subplot(spec[1, 1])
-    plt.xlabel('Time (years)',size='x-small')
-    plt.ylabel('Fraction Remaining',size='x-small')
-    plt.title("Exponential Decay of Radioactive Elements",size='x-small')
-    plt.xlim(0, 20000)
-    plt.ylim(0, 1)
-    plt.plot(x3, y31, c='red', linestyle='dashed', label='C-14')
-    plt.plot(x3, y32, c='green',linestyle='solid', label='Ra-226')
-    plt.legend()
-
+    ax21.plot(x3, y31, c='red', linestyle='dashed', label='C-14')
+    ax21.plot(x3, y32, c='green', linestyle='solid', label='Ra-226')
+    ax21.set_xlabel('Time (years)', size='x-small')
+    ax21.set_ylabel('Fraction Remaining', size='x-small')
+    ax21.set_title("Exponential Decay of Radioactive Elements", size='x-small')
+    ax21.set_xlim(0, 20000)
+    ax21.set_ylim(0, 1)
+    ax21.legend()
 
     ax3 = fig.add_subplot(spec[2, :])
-    plt.title('Project A',size='x-small')
-    plt.xlabel('Grades',size='x-small')
-    plt.ylabel('Number of Students',size='x-small')
-    plt.ylim(0, 30)
-    plt.xlim(0, 100)
-    bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    plt.hist(student_grades, bins=bins, edgecolor='black')
-    plt.xticks(np.arange(0, 110, 10))
+    ax3.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
+    ax3.set_title('Project A', size='x-small')
+    ax3.set_xlabel('Grades', size='x-small')
+    ax3.set_ylabel('Number of Students', size='x-small')
+    ax3.set_ylim(0, 30)
+    ax3.set_xlim(0, 100)
+    ax3.set_xticks(np.arange(0, 110, 10))
+
     plt.show()
