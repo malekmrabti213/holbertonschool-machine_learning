095ad8b6aae79625b0e266b2af3c855a8e2b4728
malekmrabti213 <malek6mrabti@gmail.com>
2024-04-09 13:53:29 +0100

    plot

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,12 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def bars():
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
-
-    x = ['Farrah', 'Fred', 'Felicia']    
+    x = ['Farrah', 'Fred', 'Felicia'] 
     plt.bar(x, fruit[0], color='r', label='apples', width=0.5)
     plt.bar(x, fruit[1], bottom=fruit[0], color='yellow', label='bananas',
             width=0.5)
@@ -19,8 +19,8 @@ def bars():
         color='#ff8000',
         label='oranges',
         width=0.5)
-    plt.bar(x, fruit[3], bottom=fruit[0] + fruit[1] + fruit[2], color='#ffe5b4',
-            label='peaches', width=0.5)
+    plt.bar(x, fruit[3], bottom=fruit[0] + fruit[1] + fruit[2],
+            color='#ffe5b4', label='peaches', width=0.5)
     plt.legend(loc='upper right')
     plt.ylabel('Quantity of Fruit')
     plt.title('Number of Fruit per Person')
