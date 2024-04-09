c30b52f88d4a489edeafb92eb78ae77345d1a096
malekmrabti213 <malek6mrabti@gmail.com>
2024-04-09 13:56:06 +0100

    plotting

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+"""
+Task 6
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def bars():
+    """
+    """
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
