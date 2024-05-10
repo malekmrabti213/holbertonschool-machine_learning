395d35fe366a65099d5d416a0f49df7ca2514b29
malekmrabti213 <malek6mrabti@gmail.com>
2024-05-10 23:56:35 +0100

    task 12

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+   Learning Rate decay upgraded
+"""
+
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, decay_step):
+
+    lr_schedule  = tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
+        decay_rate=decay_rate,
+        decay_steps=decay_step,
+        staircase=True) 
+    
+    return lr_schedule 
