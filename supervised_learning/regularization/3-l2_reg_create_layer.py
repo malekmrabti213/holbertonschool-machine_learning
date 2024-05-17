4e0f48b30a68a8108fd5d194113988ebb127aa13
malekmrabti213 <malek6mrabti@gmail.com>
2024-05-17 23:14:26 +0100

    task 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    Create layer with L2 regularization
+Create layer with L2 regularization
 """
 
 import tensorflow as tf
@@ -8,15 +8,14 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-        Function that creates a tensorflow layer includes 
-        L2 regularization
+    Function that creates a TensorFlow layer including L2 regularization
 
-        :param prev: tensor, output of previous layer
-        :param n: number of nodes the new layer
-        :param activation: activation function used on the layer
-        :param lambtha: L2 regularization parameter
+    :param prev: tensor, output of previous layer
+    :param n: number of nodes in the new layer
+    :param activation: activation function used on the layer
+    :param lambtha: L2 regularization parameter
 
-        :return: output of the new layer
+    :return: output of the new layer
     """
     initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
                                                         mode='fan_avg')
@@ -25,10 +24,9 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     new_layer = tf.keras.layers.Dense(n,
                                        activation=activation,
                                        kernel_initializer=initializer,
-                                       kernel_regularizer=
-                                       tf.keras.regularizers.L2(lambtha),
-                                       )
-    # apply layer to input
+                                       kernel_regularizer=tf.keras.regularizers.L2(lambtha))
+
+    # Apply layer to input
     output = new_layer(prev)
 
     return output
