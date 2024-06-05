76e46b087898b0bbbfbffa378d5e802998cc5845
malekmrabti213 <malek6mrabti@gmail.com>
2024-06-05 23:41:49 +0100

    task

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
-"""Useless comment"""
+"""
+Task 4
+"""
 from tensorflow import keras as K
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
@@ -8,50 +9,38 @@ projection_block = __import__('3-projection_block').projection_block
 
 def resnet50():
     """
-    Create the ResNet50 Model
-    :return: The ResNet50 model
     """
-    init = K.initializers.he_normal(seed=0)
-    input = K.Input(shape=(224, 224, 3))
+    inputs = K.Input(shape=(224, 224, 3))
+    he_normal = K.initializers.he_normal(seed=0)
+    X = K.layers.Conv2D(64,
+                        (7, 7),
+                        strides=(2, 2),
+                        padding='same',
+                        kernel_initializer=he_normal)(inputs)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    # X=K.layers.ReLU()(X)
 
-    conv_1 = K.layers.Conv2D(filters=64,
-                             kernel_size=(7, 7),
-                             strides=(2, 2),
-                             padding="same",
-                             kernel_initializer=init)(input)
-    norm_1 = K.layers.BatchNormalization(axis=3)(conv_1)
-    act_1 = K.layers.ReLU()(norm_1)
+    X = K.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same')(X)
 
-    max_pool_1 = K.layers.MaxPooling2D(pool_size=(3, 3),
-                                       strides=(2, 2),
-                                       padding="same")(act_1)
+    X = projection_block(X, [64, 64, 256], s=1)
+    for _ in range(2):
+        X = identity_block(X, [64, 64, 256])
 
-    pr_block_1_2x = projection_block(max_pool_1, [64, 64, 256], s=1)
-    id_block_1_2x = identity_block(pr_block_1_2x, [64, 64, 256])
-    id_block_2_2x = identity_block(id_block_1_2x, [64, 64, 256])
+    X = projection_block(X, [128, 128, 512])
+    for _ in range(3):
+        X = identity_block(X, [128, 128, 512])
 
-    pr_block_1_3x = projection_block(id_block_2_2x, [128, 128, 512])
-    id_block_1_3x = identity_block(pr_block_1_3x, [128, 128, 512])
-    id_block_2_3x = identity_block(id_block_1_3x, [128, 128, 512])
-    id_block_3_3x = identity_block(id_block_2_3x, [128, 128, 512])
+    X = projection_block(X, [256, 256, 1024])
+    for _ in range(5):
+        X = identity_block(X, [256, 256, 1024])
 
-    pr_block_1_4x = projection_block(id_block_3_3x, [256, 256, 1024])
-    id_block_1_4x = identity_block(pr_block_1_4x, [256, 256, 1024])
-    id_block_2_4x = identity_block(id_block_1_4x, [256, 256, 1024])
-    id_block_3_4x = identity_block(id_block_2_4x, [256, 256, 1024])
-    id_block_4_4x = identity_block(id_block_3_4x, [256, 256, 1024])
-    id_block_5_4x = identity_block(id_block_4_4x, [256, 256, 1024])
+    X = projection_block(X, [512, 512, 2048])
+    for _ in range(2):
+        X = identity_block(X, [512, 512, 2048])
 
-    pr_block_1_5x = projection_block(id_block_5_4x, [512, 512, 2048])
-    id_block_1_5x = identity_block(pr_block_1_5x, [512, 512, 2048])
-    id_block_2_5x = identity_block(id_block_1_5x, [512, 512, 2048])
+    X = K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1))(X)
+    Y = K.layers.Dense(1000, activation='softmax',
+                       kernel_initializer=he_normal)(X)
 
-    max_pool = K.layers.AvgPool2D(pool_size=(7, 7),
-                                  strides=(1, 1),
-                                  padding="valid")(id_block_2_5x)
-
-    dense_output = K.layers.Dense(units=1000,
-                                  activation="softmax",
-                                  kernel_initializer=init)(max_pool)
-
-    return K.models.Model(inputs=input, outputs=dense_output)
+    return K.models.Model(inputs=inputs, outputs=Y)
