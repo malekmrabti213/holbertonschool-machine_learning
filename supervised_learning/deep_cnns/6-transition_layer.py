5acf24415be52168ad3ca806999ee3151d07906d
malekmrabti213 <malek6mrabti@gmail.com>
2024-06-06 17:24:45 +0100

    task

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
-
-"""Useless comment"""
+"""
+Task 6
+"""
 from tensorflow import keras as K
 
 
 def transition_layer(X, nb_filters, compression):
     """
-    Create the transision layer of the DenseNet-c model
-    :param X: The output of the previous filter
-    :param nb_filters: The number of filter of the previous layer
-    :param compression: The compression rate
-    :return:
     """
-    init = K.initializers.he_normal(seed=0)
-    norm = K.layers.BatchNormalization()(X)
-    act = K.layers.ReLU()(norm)
-    conv_1x1 = K.layers.Conv2D(filters=int(nb_filters*compression),
-                               kernel_size=(1, 1),
-                               kernel_initializer=init)(act)
-    pooling = K.layers.AveragePooling2D(pool_size=(2, 2),
-                                        padding="same",
-                                        strides=(2, 2))(conv_1x1)
+    he_normal = K.initializers.he_normal(seed=0)
+    nb_filters = int(nb_filters * compression)
 
-    return pooling, int(nb_filters*compression)
+    # 1x1 conv
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.Conv2D(nb_filters, (1, 1),
+                        padding='same',
+                        kernel_initializer=he_normal)(X)
+
+    # average pool
+    X = K.layers.AveragePooling2D((2, 2), strides=(2, 2), padding='same')(X)
+
+    return X, nb_filters
