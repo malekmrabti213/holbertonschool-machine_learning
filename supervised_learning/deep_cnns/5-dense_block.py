e81b7a0063cbc7784bc073106e5d890ef30a49ea
malekmrabti213 <malek6mrabti@gmail.com>
2024-06-06 10:40:18 +0100

    task

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
-
-# import tensorflow.keras as K
-
-import tensorflow as tf
+"""
+Task 5
+"""
 from tensorflow import keras as K
 
-def dense_block(X, nb_filters, growth_rate, layers):
 
+def dense_block(X, nb_filters, growth_rate, layers):
+    """
+    """
+    he_normal=K.initializers.he_normal(seed=0)
     H = X
 
     for _ in range(layers):
         # 1x1 conv
         X = K.layers.BatchNormalization(axis=3)(H)
         X = K.layers.Activation('relu')(X)
-        X = K.layers.Conv2D(growth_rate * 4, (1, 1), padding='same', kernel_initializer=K.initializers.he_normal())(X)
+        X = K.layers.Conv2D(growth_rate * 4, (1, 1), padding='same', kernel_initializer=he_normal)(X)
 
         # 3x3 conv
         X = K.layers.BatchNormalization(axis=3)(X)
         X = K.layers.Activation('relu')(X)
-        X = K.layers.Conv2D(growth_rate, (3, 3), padding='same', kernel_initializer=K.initializers.he_normal())(X)
+        X = K.layers.Conv2D(growth_rate, (3, 3), padding='same', kernel_initializer=he_normal)(X)
 
         # concatenate all outputs of dense block
         H = K.layers.Concatenate(axis=3)([H, X])
