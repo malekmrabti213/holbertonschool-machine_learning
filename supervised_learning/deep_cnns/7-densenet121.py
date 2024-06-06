be2975d94257720bd3a290fe292b4aff8d35ba83
malekmrabti213 <malek6mrabti@gmail.com>
2024-06-06 17:41:29 +0100

    task

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-
-# import tensorflow.keras as K
-
-import tensorflow as tf
+"""
+Task 7
+"""
 from tensorflow import keras as K
-
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
 
+
 def densenet121(growth_rate=32, compression=1.0):
+    """
+    """
     inputs = K.Input(shape=(224, 224, 3))
 
     nb_filters = 2 * growth_rate
