2bd686496a503925f3d70df7649fc63e4b0700cd
malekmrabti213 <malek6mrabti@gmail.com>
2024-06-06 17:43:43 +0100

    task

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -10,6 +10,7 @@ transition_layer = __import__('6-transition_layer').transition_layer
 def densenet121(growth_rate=32, compression=1.0):
     """
     """
+    he_normal = K.initializers.he_normal(seed=0)
     inputs = K.Input(shape=(224, 224, 3))
 
     nb_filters = 2 * growth_rate
@@ -17,7 +18,9 @@ def densenet121(growth_rate=32, compression=1.0):
     # 7x7 conv, stride 2
     X = K.layers.BatchNormalization(axis=3)(inputs)
     X = K.layers.Activation('relu')(X)
-    X = K.layers.Conv2D(nb_filters, (7, 7), strides=2, padding='same', kernel_initializer=K.initializers.he_normal())(X)
+    X = K.layers.Conv2D(nb_filters, (7, 7), strides=2,
+                        padding='same',
+                        kernel_initializer=he_normal)(X)
 
     # 3x3 max pool, stride 2
     X = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
@@ -45,6 +48,7 @@ def densenet121(growth_rate=32, compression=1.0):
 
     # 7x7 global average pool
     X = K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1))(X)
-    Y = K.layers.Dense(1000, activation='softmax', kernel_initializer=K.initializers.he_normal())(X)
+    Y = K.layers.Dense(1000, activation='softmax',
+                       kernel_initializer=he_normal)(X)
 
     return K.models.Model(inputs=inputs, outputs=Y)
