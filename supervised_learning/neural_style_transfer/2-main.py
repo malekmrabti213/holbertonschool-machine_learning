ca72e08acc88fedf438b9563e90d5aba7c34bf5a
malekmrabti213 <malek6mrabti@gmail.com>
2024-06-26 19:17:46 +0100

    update

--- supervised_learning/neural_style_transfer/2-main.py
+++ supervised_learning/neural_style_transfer/2-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import matplotlib.image as mpimg
+import os
+import random
 import numpy as np
 import tensorflow as tf
 
@@ -8,28 +10,19 @@ NST = __import__('2-neural_style').NST
 
 
 if __name__ == '__main__':
-    style_image = mpimg.imread("C:/Users/CAMPUSNA/Desktop/ML Projects/supervised learnning/12.neural Style Transfer/starry_night.jpg")
-    content_image = mpimg.imread("C:/Users/CAMPUSNA/Desktop/ML Projects/supervised learnning/12.neural Style Transfer/golden_gate.jpg")
-    np.random.seed(0)
+    style_image = mpimg.imread("starry_night.jpg")
+    content_image = mpimg.imread("golden_gate.jpg")
+
+    # Reproducibility
+    SEED = 0
+    os.environ['PYTHONHASHSEED'] = str(SEED)
+    os.environ['TF_ENABLE_ONEDNN_OPTS']= '0'
+    random.seed(SEED)
+    np.random.seed(SEED)
+    tf.random.set_seed(SEED)
+
     nst = NST(style_image, content_image)
     input_layer = tf.constant(np.random.randn(1, 28, 30, 3), dtype=tf.float32)
+    print(input_layer)
     gram_matrix = nst.gram_matrix(input_layer)
     print(gram_matrix)
-# #### main 0
-
-# # #!/usr/bin/env python3
-
-# import numpy as np
-# import tensorflow as tf
-
-# NST = __import__('2-neural_style').NST
-
-# if __name__ == '__main__':
-#     np.random.seed(0)
-#     tf.enable_eager_execution()
-#     input_layer = tf.constant(np.random.randn(1, 320, 280, 3), dtype=tf.float32)
-#     gram_matrix = NST.gram_matrix(input_layer)
-#     print(gram_matrix)
-#     input_layer = tf.Variable(np.random.randn(1, 256, 512, 10), dtype=tf.float32)
-#     gram_matrix = NST.gram_matrix(input_layer)
-#     print(gram_matrix)
