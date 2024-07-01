453476fdcb2ea8e7fe40713f9a69118126c9431b
malekmrabti213 <malek6mrabti@gmail.com>
2024-07-01 22:38:55 +0100

    update

--- supervised_learning/neural_style_transfer/10-neural_style.py
+++ supervised_learning/neural_style_transfer/10-neural_style.py
@@ -165,8 +165,9 @@ class NST:
         """
         """
 
-        if not (isinstance(image, tf.Tensor) or isinstance(image, tf.Variable)) or \
-        not (3 <= image.shape.ndims <= 4):
+        if not (isinstance(image, tf.Tensor) or isinstance(image, tf.Variable)):
+            raise TypeError('image must be a tensor of rank 3 or 4')
+        if not 3 <= image.shape.ndims <= 4:
             raise TypeError('image must be a tensor of rank 3 or 4')
         return tf.reduce_sum(tf.image.total_variation(image))
 
