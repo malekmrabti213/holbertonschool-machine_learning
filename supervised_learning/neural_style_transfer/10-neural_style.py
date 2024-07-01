768660ce4f5025a4f52f60c9c4251faf2ef9a9c8
malekmrabti213 <malek6mrabti@gmail.com>
2024-07-01 22:10:54 +0100

    update

--- supervised_learning/neural_style_transfer/10-neural_style.py
+++ supervised_learning/neural_style_transfer/10-neural_style.py
@@ -164,9 +164,9 @@ class NST:
     def variational_cost(image):
         """
         """
-        var = image.shape.ndims
+      
         if not (isinstance(image, tf.Tensor) or
-                isinstance(image, tf.Variable)) or not 3 <= var <= 4:
+                isinstance(image, tf.Variable)) or not 3 <= image.shape.ndims <= 4:
             raise TypeError('image must be a tensor of rank 3 or 4')
         return tf.reduce_sum(tf.image.total_variation(image))
 
