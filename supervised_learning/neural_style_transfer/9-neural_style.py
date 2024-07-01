0952f63bd9cc0011e4c40c2f5136a41cfb11cafe
malekmrabti213 <malek6mrabti@gmail.com>
2024-07-01 19:07:08 +0100

    update

--- supervised_learning/neural_style_transfer/9-neural_style.py
+++ supervised_learning/neural_style_transfer/9-neural_style.py
@@ -99,8 +99,8 @@ class NST:
         """
         """
         check = (input_layer.shape.ndims != 4)
-        if not (isinstance(input_layer, tf.Tensor)
-                 or isinstance(input_layer, tf.Variable)) or check:
+        if not (isinstance(input_layer, tf.Tensor) or
+                isinstance(input_layer, tf.Variable)) or check:
             raise TypeError('input_layer must be a tensor of rank 4')
         _, nh, nw, _ = input_layer.shape.dims
         G = tf.linalg.einsum('bijc,bijd->bcd', input_layer, input_layer)
@@ -123,13 +123,13 @@ class NST:
         """
         """
         check = (style_output.shape.ndims != 4)
-        if not (isinstance(style_output, tf.Tensor)
-                 or isinstance(style_output, tf.Variable)) or check:
+        if not (isinstance(style_output, tf.Tensor) or
+                isinstance(style_output, tf.Variable)) or check:
             raise TypeError('style_output must be a tensor of rank 4')
         m, _, _, nc = style_output.shape.dims
         check1 = (gram_target.shape.dims != [m, nc, nc])
-        if not (isinstance(gram_target, tf.Tensor)
-                 or isinstance(gram_target, tf.Variable)) or check1:
+        if not (isinstance(gram_target, tf.Tensor) or
+                isinstance(gram_target, tf.Variable)) or check1:
             raise TypeError(
                 'gram_target must be a tensor of shape [{}, {}, {}]'
                 .format(m, nc, nc))
@@ -153,8 +153,8 @@ class NST:
         """
         """
         check = (content_output.shape.dims != self.content_feature.shape.dims)
-        if not (isinstance(content_output, tf.Tensor)
-                 or isinstance(content_output, tf.Variable)) or check:
+        if not (isinstance(content_output, tf.Tensor) or
+                isinstance(content_output, tf.Variable)) or check:
             raise TypeError('content_output must be a tensor of shape {}'.
                             format(self.content_feature.shape))
         _, nh, nw, nc = content_output.shape.dims
@@ -165,8 +165,8 @@ class NST:
         """
         """
         check = (generated_image.shape.dims != self.content_image.shape.dims)
-        if not (isinstance(generated_image, tf.Tensor)
-                 or isinstance(generated_image, tf.Variable)) or check:
+        if not (isinstance(generated_image, tf.Tensor) or
+                isinstance(generated_image, tf.Variable)) or check:
             raise TypeError('generated_image must be a tensor of shape {}'.
                             format(self.content_image.shape))
         preprocessed = (tf.keras.applications.
@@ -183,8 +183,8 @@ class NST:
         """
         """
         check = (generated_image.shape.dims != self.content_image.shape.dims)
-        if not (isinstance(generated_image, tf.Tensor)
-                 or isinstance(generated_image, tf.Variable)) or check:
+        if not (isinstance(generated_image, tf.Tensor) or 
+                isinstance(generated_image, tf.Variable)) or check:
             raise TypeError('generated_image must be a tensor of shape {}'.
                             format(self.content_image.shape))
         with tf.GradientTape() as tape:
