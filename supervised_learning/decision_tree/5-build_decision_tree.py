5f7ffc250eec5fce4c350380fdf228de9e360b95
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-07 18:23:56 +0100

    our ns

--- supervised_learning/decision_tree/5-build_decision_tree.py
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -164,7 +164,7 @@ class Leaf(Node):
     def __str__(self):
         """
         """
-        return f"-> leaf [value={self.value}] "
+        return f"-> leaf [value={self.value}]"
 
     def get_leaves_below(self):
         """
