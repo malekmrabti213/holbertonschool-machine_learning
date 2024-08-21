8a0ba3d3b76820913b7359b27fee6a36718e30d9
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-21 15:37:36 +0100

    we

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -5,5 +5,5 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 def tf_idf(sentences, vocab=None):
     vectorizer = TfidfVectorizer(vocabulary=vocab)
     E = vectorizer.fit_transform(sentences)
-    F = vectorizer.get_feature_names()
+    F = vectorizer.get_feature_names_out()
     return E.toarray(), F
