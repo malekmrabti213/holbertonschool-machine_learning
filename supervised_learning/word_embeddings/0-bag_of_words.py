eb4a0189e777faf59ee54780e2779d70e45aad0b
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-22 10:43:55 +0100

    we

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""
+NLP --WE --Task 0
+"""
 
 from sklearn.feature_extraction.text import CountVectorizer
 
+
 def bag_of_words(sentences, vocab=None):
+    """
+    """
     vectorizer = CountVectorizer(vocabulary=vocab)
     E = vectorizer.fit_transform(sentences)
     F = vectorizer.get_feature_names_out()
