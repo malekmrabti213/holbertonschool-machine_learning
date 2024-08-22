f58d8d054048b43027b3f762304c529f8afe9274
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-22 10:36:59 +0100

    WEm

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -5,5 +5,5 @@ from sklearn.feature_extraction.text import CountVectorizer
 def bag_of_words(sentences, vocab=None):
     vectorizer = CountVectorizer(vocabulary=vocab)
     E = vectorizer.fit_transform(sentences)
-    F = vectorizer.get_feature_names()
+    F = vectorizer.get_feature_names_out()
     return E.toarray(), F
