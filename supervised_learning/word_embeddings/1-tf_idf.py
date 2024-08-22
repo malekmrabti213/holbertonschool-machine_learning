074a5dd4833da846c6abbc181143c79a1064667f
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-22 10:56:59 +0100

    we

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""
+NLP --WE --Task 1
+"""
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 def tf_idf(sentences, vocab=None):
+    """
+    """
     vectorizer = TfidfVectorizer(vocabulary=vocab)
     E = vectorizer.fit_transform(sentences)
     F = vectorizer.get_feature_names_out()
