a00fc5b5f509498a367980cab8e4e8a287a9b057
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-23 20:08:52 +0100

    cp

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -10,7 +10,7 @@ def fasttext_model(sentences, vector_size=100, min_count=5,
                    window=5, negative=5, cbow=True,
                    epochs=5, seed=0, workers=1):
     """
-    """      
+    """     
     # Set the training algorithm based on cbow parameter
     sg = 0 if cbow else 1
 
