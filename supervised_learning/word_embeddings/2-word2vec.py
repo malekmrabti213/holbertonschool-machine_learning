f11feaa3ed1ea380a8c182bb42dfb8e7e531b843
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-23 16:29:17 +0100

    doc

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
 """
-    Train Word2Vec
+    NLP --WE --Task2 --Train Word2Vec
 """
+
 from gensim.models import Word2Vec
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
-            creates and trains a gensim word2vec model
+    Creates and trains a gensim word2vec model.
 
     :param sentences: list of sentences to be trained on
+    :type sentences: list of list of str
     :param vector_size: dimensionality of the embedding layer
-    :param min_count: minimum number of occurrences of a word
-        for use in training
-    :param window: maximum distance between the current and predicted
-    word within a sentence
+    :type vector_size: int
+    :param min_count: minimum number of occurrences of a word for use in training
+    :type min_count: int
+    :param window: maximum distance between the current and predicted word within a sentence
+    :type window: int
     :param negative: size of negative sampling
-    :param cbow: boolean to determine training type: True=CBOW, False=Skip-gram
+    :type negative: int
+    :param cbow: boolean to determine training type; True for CBOW, False for Skip-gram
+    :type cbow: bool
     :param epochs: number of iterations to train over
+    :type epochs: int
     :param seed: seed for the random number generator
+    :type seed: int
     :param workers: number of worker threads to train the model
+    :type workers: int
 
-    :return: trained model
+    :return: Trained Word2Vec model
+    :rtype: gensim.models.Word2Vec
     """
     if cbow is True:
         sg = 0
