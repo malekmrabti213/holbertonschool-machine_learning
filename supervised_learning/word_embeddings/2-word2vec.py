aa47a8ebeb306cc4436833a1b5b968c8607f1db4
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-23 16:49:03 +0100

    test

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -11,9 +11,9 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     Creates and trains a Word2Vec model using the gensim library.
 
     Args:
-        sentences (list of list of str): List of tokenized sentences to be trained on.
+        sentences: List of tokenized sentences to be trained on.
         vector_size: Dimensionality of the word vectors. Defaults to 100.
-        min_count: Ignores all words with total frequency lower than this. Defaults to 5.
+        min_count: Ignores all words with total frequency lower than this.
         window: Maximum distance between the current and predicted word
         negative: If > 0, negative sampling will be used
         epochs: Number of iterations over the corpus. Defaults to 5.
