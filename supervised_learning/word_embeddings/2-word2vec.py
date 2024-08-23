55c4796e4de679c5c2a6f3b32a898fbc797007a2
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-23 16:47:21 +0100

    doc

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
-
 """
-NLP --WE --Task2 --Train Word2Vec
+Train a Word2Vec model using gensim.
 """
-
 from gensim.models import Word2Vec
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
+    Creates and trains a Word2Vec model using the gensim library.
 
+    Args:
+        sentences (list of list of str): List of tokenized sentences to be trained on.
+        vector_size: Dimensionality of the word vectors. Defaults to 100.
+        min_count: Ignores all words with total frequency lower than this. Defaults to 5.
+        window: Maximum distance between the current and predicted word
+        negative: If > 0, negative sampling will be used
+        epochs: Number of iterations over the corpus. Defaults to 5.
+        seed: Seed for the random number generator. Defaults to 0.
+        workers: Use these many worker threads to train the model
+
+    Returns:
+        gensim.models.Word2Vec: The trained Word2Vec model.
     """
-    if cbow is True:
-        sg = 0
-    else:
-        sg = 1
+    sg = 0 if cbow else 1
+
     model = Word2Vec(sentences=sentences,
                      vector_size=vector_size,
                      window=window,
@@ -24,6 +33,6 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                      seed=seed,
                      workers=workers,
                      epochs=epochs,
-                     sg=cbow)
+                     sg=sg)
 
     return model
