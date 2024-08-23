48e791fcdfed3fcd793b4aef526bf6063c2166e0
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-23 16:38:22 +0100

    doc

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 NLP --WE --Task2 --Train Word2Vec
 """
@@ -9,11 +10,13 @@ from gensim.models import Word2Vec
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
+    
     """
     if cbow is True:
         sg = 0
     else:
         sg = 1
+
     model = Word2Vec(sentences=sentences,
                      vector_size=vector_size,
                      window=window,
