b3f822f74d96c9ed96265a23b4015cea7f2a2335
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-23 17:38:34 +0100

    up

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
+"""
+NLP --WE --Task 2
+"""
+
 from gensim.models import Word2Vec
 
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5, cbow=True, epochs=5, seed=0, workers=1):
+
+def word2vec_model(sentences, vector_size=100, min_count=5,
+                   window=5, negative=5, cbow=True,
+                   epochs=5, seed=0, workers=1):
+    """
+    """
     # Set the training algorithm based on cbow parameter
     sg = 0 if cbow else 1
-    
+
     # Create the Word2Vec model
     model = Word2Vec(
         sentences=sentences,
@@ -17,9 +26,12 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5
         seed=seed,
         workers=workers    
     )
+
     # prepare the model vocabulary
     model.build_vocab(sentences)
+
     # Train the model
-    model.train(sentences, total_examples=model.corpus_count, epochs=model.epochs)
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
 
     return model
