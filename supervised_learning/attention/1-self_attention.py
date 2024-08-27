502271aeb700ef90e4388beff1e6230031fe4a0f
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-27 12:30:12 +0100

    att

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
-
+"""
+Task 1
+"""
 import tensorflow as tf
 
+
 class SelfAttention(tf.keras.layers.Layer):
+    """
+    """
     def __init__(self, units):
         super(SelfAttention, self).__init__()
+        """
+        """
         self.W = tf.keras.layers.Dense(units)
         self.U = tf.keras.layers.Dense(units)
         self.V = tf.keras.layers.Dense(1)
 
     def call(self, s_prev, hidden_states):
+        """
+        """
         s_prev_exp = tf.expand_dims(s_prev, 1)
         score = self.V(tf.nn.tanh(self.W(s_prev_exp) + self.U(hidden_states)))
         weights = tf.nn.softmax(score, axis=1)
