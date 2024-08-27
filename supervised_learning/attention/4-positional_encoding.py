a8932524127ae8c8122572cc1509f72f16bf0c06
malekmrabti213 <malek6mrabti@gmail.com>
2024-08-27 15:47:30 +0100

    att

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-
+"""
+Task 4
+"""
 import numpy as np
 
+
 def positional_encoding(max_seq_len, dm):
+    """
+    """
     positions = np.arange(max_seq_len)[:, np.newaxis]
     dimensions = np.arange(dm)[np.newaxis, :]
     angles = positions / (10000 ** (2 * (dimensions // 2) / np.float32(dm)))
