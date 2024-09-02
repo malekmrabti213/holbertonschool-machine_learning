cd4b65b58ad3280c7b2226541de324aa8a4349c6
malekmrabti213 <malek6mrabti@gmail.com>
2024-09-02 18:17:49 +0100

    &àà

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,14 +1,5 @@
--- Function that makes a safe divison
-
-DELIMITER $$
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv(
-    a INT,
-    b INT
-)
-RETURNS FLOAT
-BEGIN
-    RETURN IF(b = 0, 0, a /b);
-END$$
-
-DELIMITER ;
+-- Write a SQL script that creates a function SafeDiv that divides (and returns)
+-- the first by the second number or returns 0 if the second number is equal to 0.
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+RETURN IF(b=0,0,a/b);
