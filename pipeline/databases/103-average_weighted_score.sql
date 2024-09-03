8a81eec833d1fa444393258665ce8f9aa91abe74
malekmrabti213 <malek6mrabti@gmail.com>
2024-09-03 18:34:23 +0100

    aadd

--- pipeline/databases/103-average_weighted_score.sql
+++ pipeline/databases/103-average_weighted_score.sql
@@ -1,15 +1,14 @@
--- Creates a stored procedure ComputeAverageWeightedScoreForUser that computes
---    and stores the average score for a student
--- Procedure AddBonus takes 1 input:
---    user_id, a users.id value, can assume user_id is linked to existing users
+-- SQL COMMENT
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id_new INTEGER)
-BEGIN
-	UPDATE users SET average_score=(
-	SELECT SUM(score * weight) / SUM(weight) FROM corrections
-	JOIN projects
-	ON corrections.project_id=projects.id
-	WHERE user_id=user_id_new);
-END; //
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
+    BEGIN
+        SET @avg = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight) FROM corrections
+            LEFT JOIN projects ON projects.id = corrections.project_id
+            WHERE corrections.user_id = user_id);
+        UPDATE users SET average_score = @avg WHERE id = user_id;
+    END; //
+
 DELIMITER ;
