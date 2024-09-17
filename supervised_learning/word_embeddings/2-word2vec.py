0461afb96dac24979df54a4b75ccef997b46c6ed
malekmrabti213 <malek6mrabti@gmail.com>
2024-09-17 13:49:42 +0100

    ss

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -4,6 +4,7 @@ Word2Vec model training
 """
 import gensim 
 
+
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
