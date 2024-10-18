d7aa0177740e51166dfe63c63feed3c2c4535fc1
malekmrabti213 <malek6mrabti@gmail.com>
2024-10-18 17:54:32 +0100

    rl

--- reinforcement_learning/q_learning/0-main.py
+++ reinforcement_learning/q_learning/0-main.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
 load_frozen_lake = __import__('0-load_env').load_frozen_lake
-import numpy as np
 
-np.random.seed(0)
 env = load_frozen_lake()
-print(env.desc)
-print(env.P[0][0])
+print(env.unwrapped.desc) 
+print(len(env.unwrapped.P[0][0]))
+print(env.unwrapped.P[0][0])
+
 env = load_frozen_lake(is_slippery=True)
-print(env.desc)
-print(env.P[0][0])
+print(env.unwrapped.desc)
+print(len(env.unwrapped.P[0][0]))
+print(env.unwrapped.P[0][0])
+
 desc = [['S', 'F', 'F'], ['F', 'H', 'H'], ['F', 'F', 'G']]
 env = load_frozen_lake(desc=desc)
-print(env.desc)
+print(env.unwrapped.desc)
 env = load_frozen_lake(map_name='4x4')
-print(env.desc)
+print(env.unwrapped.desc)
--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-module containing function train
+3-q_learning
 """
+import gym
 import numpy as np
 epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
 
@@ -10,59 +11,37 @@ def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
           epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
     """
     Function that performs Q-learning
-    
-    Args:
-    env: The environment object.
-    Q: The Q-table to update.
-    episodes: The total number of episodes to train.
-    max_steps: Max steps per episode.
-    alpha: Learning rate.
-    gamma: Discount factor for future rewards.
-    epsilon: Initial epsilon for the epsilon-greedy policy.
-    min_epsilon: Minimum value of epsilon after decay.
-    epsilon_decay: The rate at which epsilon decays.
-
-    Returns:
-    Q: Updated Q-table.
-    rewards: List of rewards per episode.
     """
     rewards = []
     initial_epsilon = epsilon
 
     for episode in range(episodes):
-        # Reset the environment to get the initial state
         state = env.reset()[0]
         terminated = False
         truncated = False
-        # done = terminated or truncated
+        done = terminated or truncated  
         total_rewards = 0
 
-        # Iterate through the steps within an episode
         for _ in range(max_steps):
-            # Select action using epsilon-greedy policy
             action = epsilon_greedy(Q, state, epsilon)
-
-            # Execute action in the environment
             new_state, reward, terminated, truncated, _ = env.step(action)
-            # Penalize for terminating the episode with zero reward
-            if terminated and reward == 0:
+
+            if done and reward == 0:
                 reward = -1
 
-            # Update Q-value using the Q-learning update rule
-            Q[state, action] = Q[state, action] + alpha * (
-                reward + gamma * np.max(Q[new_state, :]) - Q[state, action])
+            Q[state, action] = Q[state, action] + alpha * (reward + gamma *\
+                np.max(Q[new_state, :]) - Q[state, action])
 
             total_rewards += reward
             state = new_state
 
-            # Break if the episode has terminated or truncated
-            if terminated or truncated :
+            if done == True:
                 break
 
-        # Decay epsilon to balance exploration and exploitation
-        epsilon = max(min_epsilon, initial_epsilon - epsilon_decay * (episode + 1))
+        epsilon = (min_epsilon + (initial_epsilon - min_epsilon) *
+                    np.exp(-epsilon_decay * episode))
 
-        # Store total reward for this episode
+        # epsilon = max(min_epsilon, initial_epsilon - epsilon_decay * (episode + 1))
         rewards.append(total_rewards)
 
     return Q, rewards
