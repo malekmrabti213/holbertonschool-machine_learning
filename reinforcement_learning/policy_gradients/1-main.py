451aeddb433953fc177670b48db397be460efddb
malekmrabti213 <malek6mrabti@gmail.com>
2024-11-06 01:23:16 +0100

    rl

--- reinforcement_learning/policy_gradients/1-main.py
+++ reinforcement_learning/policy_gradients/1-main.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
+
 import gymnasium as gym
 import numpy as np
+import random
 policy_gradient = __import__('policy_gradient').policy_gradient
 
+def set_seed(env, seed=0):
+    env.reset(seed=seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
 env = gym.make('CartPole-v1')
-np.random.seed(0)
+set_seed(env, 0)
 
 weight = np.random.rand(4, 2)
-state , _ = env.reset(seed=0)
+state , _ = env.reset()
 print(weight)
 print(state)
-# print(type(state))
-
 
 action, grad = policy_gradient(state, weight)
 print(action)
--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -8,11 +8,29 @@ import numpy as np
 
 def policy(state, weight):
     """
-
     """
     z = state.dot(weight)
-    # Apply numerical stability fix by subtracting max of z
     z -= np.max(z)
     exp = np.exp(z)
 
     return exp / np.sum(exp)
+
+# Vectorized softmax Jacobian
+def softmax_grad(softmax):
+    """
+    """
+    s = softmax.reshape(-1,1)
+
+    return np.diagflat(s) - np.dot(s, s.T)
+
+def policy_gradient(state, weight):
+    """
+    """
+    probs = policy(state, weight)
+    action = np.random.choice(len(probs), p=probs)
+    dsoftmax = softmax_grad(probs)[action, :]
+    dlog = dsoftmax / probs[action]
+    state = state.reshape(-1, 1)
+    grad = state.dot(dlog[None, :])
+
+    return action, grad
