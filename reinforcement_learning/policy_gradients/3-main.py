fe9606db20071e1d2882ce3df5876000dbd363ae
malekmrabti213 <malek6mrabti@gmail.com>
2024-11-06 22:48:53 +0100

    rl

--- reinforcement_learning/policy_gradients/3-main.py
+++ reinforcement_learning/policy_gradients/3-main.py
@@ -3,12 +3,17 @@
 import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 train = __import__('train').train
 
-np.random.seed(0)
+def set_seed(env, seed=0):
+    env.reset(seed=seed)
+    np.random.seed(seed)
+    random.seed(seed)
 
 env = gym.make('CartPole-v1', render_mode="human")
+set_seed(env, 0)
 
-scores = train(env, 10, 0.000045, 0.98, True)
+scores = train(env, 10000, 0.000045, 0.98, True)
 
 env.close()
