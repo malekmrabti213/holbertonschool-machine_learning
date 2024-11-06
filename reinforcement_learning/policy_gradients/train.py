ef9d58a9799422fe09e4931dcfdc8d5bd9da03c2
malekmrabti213 <malek6mrabti@gmail.com>
2024-11-06 21:58:17 +0100

    up

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -5,6 +5,7 @@
 import numpy as np
 policy_gradient = __import__('policy_gradient').policy_gradient
 
+
 def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
 
     scores = []
